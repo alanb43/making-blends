97f90972a300d110cf732f1f27d47b689157eefa
Alan Bergsneider <ab43work@gmail.com>
2020-10-24 15:59:37 -0400

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -3,17 +3,14 @@
 #include <string> 
 #include <algorithm> // std::min
 
-/* A lot of the coments I write are because of new concepts I use, I sometimes write a short
-explanation so I can remember the logic. Not necessarily for readability :) */
-
 using std::cout, std::cin, std::endl, std::string, std::istringstream;
 
 string CheckForOverlap(string word1, string word2) {
   
-  int max_overlap = std::min(word1.size(), word2.size()); // std::min finds min size in 2 words
+  int max_overlap = std::min(word1.size(), word2.size()); 
   for (int i = max_overlap; i > 0; i--) { // iterates from largest possible overlap to smallest
     if (word1.substr(word1.size() - i) == word2.substr(0, i)) { // if there is an overlap
-      return word2.substr(0, i); // returns the substr that matches
+      return word2.substr(0, i); // returns a substring of the overlap
     } 
   } 
   return "";  // if no overlap is found
@@ -21,8 +18,8 @@ string CheckForOverlap(string word1, string word2) {
 
 string Squeeze(string word1, string word2) {
   string overlap = CheckForOverlap(word1, word2); // find substr of overlap
-  if (overlap == "") { // if there isn't an overlap
-    return word1 + " " + word2; // return a string of how the words word appear in the output
+  if (overlap == "") { 
+    return word1 + " " + word2; 
   } // the above/below string is what is then compared to the next word in Compress
   return word1 + word2.substr(overlap.size()); // returns the squeeze of the two words
 }
@@ -39,8 +36,8 @@ string Compress(string line) {
 
 int main() {
   string line;
-  while (getline(cin, line)) { // get each line
-    string compressed_line = Compress(line); // compress calls other functions
+  while (getline(cin, line)) { 
+    string compressed_line = Compress(line);
     cout << compressed_line << endl;  
   }
   return 0;
